a26fb76af4e18a9625a64a69751b18e4f1499ae8
Alia Eshaq <alia@greenbeeinsights.com>
2023-02-22 17:03:15 +0000

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+chinese_place = {name: "Chinese Place", address: "7 Boundary St, London E2 7JE", phone_number: 123456978, category: "chinese"}
+italian_place = {name: "Italian Place", address: "7 Boundary St, London E2 7JE", phone_number: 123456978, category: "italian"}
+japanese_place = {name: "Japanese Place", address: "7 Boundary St, London E2 7JE", phone_number: 123456978, category: "japanese"}
+french_place = {name: "French Place", address: "7 Boundary St, London E2 7JE", phone_number: 123456978, category: "french"}
+belgian_place = {name: "Belgian Place", address: "7 Boundary St, London E2 7JE", phone_number: 123456978, category: "belgian"}
+[chinese_place, italian_place, french_place, belgian_place, japanese_place].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
